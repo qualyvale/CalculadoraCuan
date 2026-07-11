78894ed9ce7bb0b4368c8c4fb3bb42009979f643
qualyvale <lamonie51@gmail.com>
2026-07-11 13:13:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let valorAtual = '';
 let operador = '';
 let valorAnterior = '';
 let resultado = '';
+let reconhecimentoVoz = null;
 
 function adicionarValor(valor) {
     if (resultado !== '') {
@@ -105,4 +106,63 @@ function limpar() {
     operador = '';
     resultado = '';
     document.getElementById('display').value = '';
+}
+
+function iniciarReconhecimentoVoz() {
+    if (reconhecimentoVoz === null) {
+        reconhecimentoVoz = new webkitSpeechRecognition();
+        reconhecimentoVoz.lang = 'pt-BR';
+        reconhecimentoVoz.maxResults = 10;
+        reconhecimentoVoz.onresult = eventoReconhecimentoVoz;
+        reconhecimentoVoz.start();
+    }
+}
+
+function eventoReconhecimentoVoz(evento) {
+    let textoReconhecido = evento.results[0][0].transcript;
+    textoReconhecido = textoReconhecido.toLowerCase();
+    if (textoReconhecido.includes('sete')) {
+        adicionarValor(7);
+    } else if (textoReconhecido.includes('oito')) {
+        adicionarValor(8);
+    } else if (textoReconhecido.includes('nove')) {
+        adicionarValor(9);
+    } else if (textoReconhecido.includes('quatro')) {
+        adicionarValor(4);
+    } else if (textoReconhecido.includes('cinco')) {
+        adicionarValor(5);
+    } else if (textoReconhecido.includes('seis')) {
+        adicionarValor(6);
+    } else if (textoReconhecido.includes('um')) {
+        adicionarValor(1);
+    } else if (textoReconhecido.includes('dois')) {
+        adicionarValor(2);
+    } else if (textoReconhecido.includes('três')) {
+        adicionarValor(3);
+    } else if (textoReconhecido.includes('zero')) {
+        adicionarValor(0);
+    } else if (textoReconhecido.includes('mais')) {
+        adicionarOperador('+');
+    } else if (textoReconhecido.includes('menos')) {
+        adicionarOperador('-');
+    } else if (textoReconhecido.includes('vezes')) {
+        adicionarOperador('*');
+    } else if (textoReconhecido.includes('dividir')) {
+        adicionarOperador('/');
+    } else if (textoReconhecido.includes('igual')) {
+        calcular();
+    } else if (textoReconhecido.includes('seno')) {
+        calcularSen();
+    } else if (textoReconhecido.includes('cosseno')) {
+        calcularCos();
+    } else if (textoReconhecido.includes('tangente')) {
+        calcularTan();
+    } else if (textoReconhecido.includes('logaritmo')) {
+        calcularLog();
+    } else if (textoReconhecido.includes('raiz')) {
+        calcularRaiz();
+    } else if (textoReconhecido.includes('limpar')) {
+        limpar();
+    }
+    reconhecimentoVoz = null;
 }
